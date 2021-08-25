ab50664f8d4f4281abb27adec3f6c246cea50a0f
Andrii Davydov <webintoit@gmail.com>
2021-08-25 19:51:02 +0300

    webpackify

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob = require('glob');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    entry: {
-        'iframe-api': './src/index.js',
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: '[name].js',
-        library: 'Proficonf',
-        libraryTarget: 'umd',
-        globalObject: 'this'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Proficonf Embedded Room Example',
-            template: './example/index.html'
-        })
-    ],
-    resolve: {
-        extensions: ['.js']
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+module.exports = (_, argv) => {
+    const isTesting = argv.test === 'true';
+
+    const config = {
+        entry: isTesting
+                ? glob.sync(__dirname + '/spec/**/*.spec.js')
+                : {
+                    'iframe-api': './src/index.js',
+                },
+        mode: 'development',
+        devtool: 'inline-source-map',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+            filename: '[name].js',
+            library: 'Proficonf',
+            libraryTarget: 'umd',
+            globalObject: 'this'
         },
-        port: 9001
-    },
-    cache: false
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Proficonf Embedded Room Example',
+                template: './example/index.html'
+            })
+        ],
+        resolve: {
+            extensions: ['.js']
+        },
+        module: {
+            rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+                }
+            }
+            ]
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            port: 9001
+        },
+        cache: false,
+        optimization: {
+            removeAvailableModules: true,
+            removeEmptyChunks: true,
+            splitChunks:  false,
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    parallel: true,
+                    extractComments: true,
+                })
+            ]
+        }
+    };
+    
+    return config;
 };
